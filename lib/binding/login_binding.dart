8a50f9f612a042e2a32d52e8efe1d78d4d8b4816
parth <dattaniparth2@gmail.com>
2023-04-28 16:17:21 +0530

    aadd

--- lib/binding/login_binding.dart
+++ lib/binding/login_binding.dart
@@ -5,6 +5,6 @@ import '../controller/controller.dart';
 class LoginBinding extends Bindings{
   @override
   void dependencies() {
-    Get.put<LoginController>(LoginController(), permanent: false);
+    Get.lazyPut(() => LoginController());
   }
 }
